47072ca0af4ecff000ad6bd4650c2f5f01f99438
Baggsy <36273471+Baggsy@users.noreply.github.com>
2018-04-19 16:21:57 +0200

    Added a commented example line for LSTM calling

--- main.py
+++ main.py
@@ -171,7 +171,11 @@ print in_shape
 # ______________________________________________________
 
 
-
+# LSTM(units, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True, kernel_initializer='glorot_uniform', 
+#             recurrent_initializer='orthogonal', bias_initializer='zeros', unit_forget_bias=True, kernel_regularizer=None, 
+#             recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, 
+#             recurrent_constraint=None, bias_constraint=None, dropout=0.0, recurrent_dropout=0.0, implementation=1, 
+#             return_sequences=False, return_state=False, go_backwards=False, stateful=False, unroll=False)
 
 #
 for unit in units:
